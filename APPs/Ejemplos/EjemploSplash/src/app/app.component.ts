8003048c61a74519ec9bc73d35543a2ace933b66
Joaquin Enriquez <joaquin.enriquez.tc@gmail.com>
2019-09-24 14:07:18 -0300

    asd

--- APPs/Ejemplos/EjemploSplash/src/app/app.component.ts
+++ APPs/Ejemplos/EjemploSplash/src/app/app.component.ts
@@ -4,12 +4,17 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { timer } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  showSplash = true; // <-- show animation
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -22,6 +27,9 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      timer(3000).subscribe(() => this.showSplash = false);
+
     });
   }
 }
