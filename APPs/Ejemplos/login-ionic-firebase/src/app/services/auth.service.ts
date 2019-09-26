d0702bb6ab0b7fd15e23a8bef3c65909d611ef41
Joaquin <joaquin.enriquez.tc@gmail.com>
2019-09-26 18:56:45 -0300

    asd

--- APPs/Ejemplos/login-ionic-firebase/src/app/services/auth.service.ts
+++ APPs/Ejemplos/login-ionic-firebase/src/app/services/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { 
+    afAuth.authState.subscribe()
+  }
 }
